36d442be6c32ecd0098331b25cda4e666cd77bf3
T. Felipe <eusouored@hotmail.com>
2023-04-15 22:23:50 -0300

    Update main.js

--- Calculadora/js/main.js
+++ Calculadora/js/main.js
@@ -1,61 +1,60 @@
 function Calculadora() {
-    return {
-        display: document.querySelector('#calc'),
-        symbolsArray: ['C', '(', ')', '/', '.', '↩', '*', '+', '-', '=', 'Backspace'],
-        numArray: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
 
-        Init() {
+        this.display = document.querySelector('#calc')
+        this.symbolsArray = ['C', '(', ')', '/', '.', '↩', '*', '+', '-', '=', 'Backspace']
+        this.numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        this.resultTrue = false
+
+        this.Init = () => {
             this.ListenerKey()
             this.computeClick()
-        },
+        }
 
-        ListenerKey() {
-            document.addEventListener('keyup', (e) => {
-                if (e.key in this.symbolsArray === false || e.key in this.numArray === false) {
-                    console.log(e.key)
-                    this.display.blur()
-                }
+        this.ListenerKey = () => {
+            document.addEventListener('keydown', (e) => {
+                if (e.key in this.symbolsArray === false || e.key in this.numArray === false) this.display.blur()
             })
 
             this.display.addEventListener('keypress', (e) => {
-                if (e.key in this.symbolsArray === false || (e.key in this.numArray === false)) {
-                this.display.blur()
-                }
+                if (e.key in this.symbolsArray === false || e.key in this.numArray === false) this.display.blur()
             })
-        },
+        }
 
-        computeClick() {
+        this.computeClick = () => {
             document.addEventListener('click', (e) => {
                 const el = e.target
                 if (el.classList.contains('btn')){
-                    if (el.innerText == 'C') {
-                        this.Limpar()
-                    } else if (el.innerText == '=') {
-                        this.Result() 
-                    } else if (el.innerText == '↩' ){
-                        this.del_case()
-                    } else{ 
-                        this.display.value += el.innerText
-                    }
+                    if (el.innerText == 'C')this.Limpar() 
+                    else if (el.innerText == '=') this.Result() 
+                    else if (el.innerText == '↩' ) this.del_case() 
+                    else {
+                        if (this.resultTrue) {
+                            if (el.innerText in this.symbolsArray) this.display.value += el.innerText
+                            else this.display.value = ''
+                            this.resultTrue = false
+                        }
+                        }
                 }
 
                 
             })
-        },
+        }
         
-        Limpar() {
+        this.Limpar = () => {
             this.display.value = ''
-        },
+        }
 
-        del_case() {
+        this.del_case = () => {
             this.display.value = String(this.display.value).slice(0, -1)
-        }, 
+        }
 
-        Result() {
+        this.Result = () => {
             try{
                 const geval = eval(this.display.value)
-                if (typeof geval == "number") this.display.value = eval(this.display.value)
-                else alert('Conta inválida!')
+                if (typeof geval == "number") {
+                    this.display.value = eval(this.display.value)
+                    this.resultTrue = true
+                } else alert('Conta inválida!')
             } catch (e) {
                 alert('Conta inválida!')
                 this.display.value = ''
@@ -63,9 +62,8 @@ function Calculadora() {
             }
             
         }
-    }
 }
 
-calc = Calculadora()
+calc =  new Calculadora()
 calc.Init()
 
