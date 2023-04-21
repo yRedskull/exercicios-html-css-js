fa1653e63c09996f96a2825452562559feeb567f
T. Felipe <eusouored@hotmail.com>
2023-04-21 11:52:19 -0300

    Update main.js

--- exercicios/ex25/main.js
+++ exercicios/ex25/main.js
@@ -7,5 +7,5 @@ const numeros = [5, 80, 20, 60, 4, 2, 1, 3, 7, 68, 422]
 const numerosPares = numeros
     .filter(valor => valor % 2 === 0)
     .map(valor => valor * 2)
-    .reduce((soma, valor) => soma += valor)
+    .reduce((soma, valor) => soma += valor, 0)
 console.log(numerosPares)
