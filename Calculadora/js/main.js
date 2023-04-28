b44050240288df1b0e6672834e84eb140dbf1cb5
T. Felipe <eusouored@hotmail.com>
2023-04-28 08:40:41 -0300

    Calculadora

--- Calculadora/js/main.js
+++ Calculadora/js/main.js
@@ -1,83 +1,106 @@
-function Calculadora() {
-
+class Calculadora {
+    constructor() {
         this.display = document.querySelector('#calc')
-        this.symbolsArray = ['C', '(', ')', "/", '.', '↩', '*', '+', '-', '=', 'Backspace']
+        this.symbolsTextArray = ['C', '(', ')', "/", '.', '↩', '*', '+', '-', '=', 'Backspace']
+        this.symbolsIdObj = {dvs: 111, x: 106, plus: 107, minus: 109, dot: 110, equal: 13}
         this.numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-        this.resultTrue = false
+        this.resultTrue = false}
 
-        this.Init = () => {
-            this.ListenerKey()
-            this.computeClick()
-        }
+    init = () => {
+        this.ListenerKey()
+        this.computeClick()
+    }
 
-        this.ListenerKey = () => {
-            document.addEventListener('keydown', (e) => {
-                this.display.focus()
-                if (e.key in this.symbolsArray 
-                    || e.key in this.numArray 
-                    || e.keyCode == 8) return 
-                console.log(e.key in this.symbolsArray, e )
-                this.display.blur() 
-                
-            })
-
-            this.display.addEventListener('keypress', (e) => {
-                if (e.key in this.symbolsArray === false || e.key in this.numArray === false) this.display.blur()
-            })
-        }
-
-        this.computeClick = () => {
-            document.addEventListener('click', (e) => {
-                const el = e.target
-                if (el.classList.contains('btn')){
-                    if (el.innerText == 'C') this.Limpar() 
-                    else if (el.innerText == '=') this.Result() 
-                    else if (el.innerText == '↩' ) this.del_case() 
-                    else {
-                        if (this.resultTrue) {
-                            if (el.innerText in this.numArray) {
-                                this.resultTrue = false
-                                return this.display.value = el.innerText
-                            } else{
-                            this.display.value += el.innerText
-                            this.resultTrue = false
-                        }
-                        } else {
-                            this.display.value += el.innerText
-                        }
-                        }
-                }
-
-                
-            })
-        }
-        
-        this.Limpar = () => {
-            this.display.value = ''
-            this.resultTrue = false
-        }
-
-        this.del_case = () => {
-            this.display.value = String(this.display.value).slice(0, -1)
-            this.resultTrue = false
-        }
-
-        this.Result = () => {
-            try{
-                const geval = eval(this.display.value)
-                if (typeof geval == "number") {
-                    this.display.value = eval(this.display.value)
-                    this.resultTrue = true
-                } else alert('Conta inválida!')
-            } catch (e) {
-                alert('Conta inválida!')
-                this.display.value = ''
-                
+    ListenerKey = () => {
+        document.addEventListener('keydown', (e) => {
+            this.display.focus()
+            if (this.resultTrue) {
+                if (this.numArray.includes(e.key)) this.display.value = ''
             }
+            if (Object.values(this.symbolsIdObj).includes(e.keyCode)) {
+                for (let chave of Object.keys(this.symbolsIdObj)) {
+                    if (this.symbolsIdObj[chave] === e.keyCode) {
+                        const sym = document.getElementById(chave)
+                        sym.classList.add("hover-btn")
+                        setTimeout(() => {
+                            sym.classList.remove("hover-btn")
+                        }, 200)
+                        if (e.keyCode === 13) return this.result()
+                        return
+                        
+                    }
+                }
+            } else if (e.key in this.numArray ){
+                const sym = document.getElementById(`n${e.key}`)
+                sym.classList.add("hover-btn")
+                setTimeout(() => {
+                    sym.classList.remove("hover-btn")
+                }, 200)
+                return 
+                    
+            } else if ( e.keyCode == 8) return
+
+            this.display.blur() 
+            
+        })
+
+        this.display.addEventListener('keypress', (e) => {
+            if (e.key === this.display.value.slice(-1)) this.display.blur()
+        })
+    }
+
+    computeClick = () => {
+        document.addEventListener('click', (e) => {
+            const el = e.target
+            if (el.classList.contains('btn')){
+                if (el.innerText == 'C') this.clear() 
+                else if (el.innerText == '=') this.result() 
+                else if (el.innerText == '↩' ) this.del_case() 
+                else {
+                    if (this.resultTrue) {
+                        if (el.innerText in this.numArray) {
+                            this.resultTrue = false
+                            return this.display.value = el.innerText
+                        } else{
+                        this.display.value += el.innerText
+                        this.resultTrue = false
+                    }
+                    } else {
+                        this.display.value += el.innerText
+                    }
+                    }
+            }
+
+            
+        })
+    }
+    
+    clear = () => {
+        this.display.value = ''
+        this.resultTrue = false
+    }
+
+    del_case = () => {
+        this.display.value = String(this.display.value).slice(0, -1)
+        this.resultTrue = false
+    }
+
+    result = () => {
+        try{
+            const geval = eval(this.display.value)
+            if (typeof geval == "number") {
+                this.display.value = eval(this.display.value)
+                this.resultTrue = true
+            } else alert('Conta inválida!')
+        } catch (e) {
+            alert('Conta inválida!')
+            this.display.value = ''
             
         }
+        
+    }
 }
 
 calc =  new Calculadora()
-calc.Init()
+calc.init()
 
